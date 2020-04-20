b8bf2219c0cb3d0b53c980dd82116c0eabad8ab5
AlexRatmansky <alexratmansky@yandex.ru>
2020-04-21 00:23:09 +0300

    web ui

--- functions/src/util/auth.ts
+++ functions/src/util/auth.ts
@@ -15,31 +15,30 @@ export interface UserRequest extends Request {
 }
 
 export const auth = async (request: UserRequest, response: Response, next) => {
-  const isValidToken = request.headers.authorization && request.headers.authorization.startsWith('Bearer ');
-
-  if (!isValidToken) {
+  let idToken;
+  if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+    idToken = request.headers.authorization.split('Bearer ')[1];
+  } else {
     console.error('No token found');
     return response.status(403).json({ error: 'Unauthorized' });
   }
-
-  const idToken = request.headers.authorization.split('Bearer ')[1];
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-
-    const dbUser = await db
-      .collection('users')
-      .where('userId', '==', request.user.uid)
-      .limit(1)
-      .get();
-
-    request.user = decodedToken;
-
-    request.user.username = dbUser.docs[0].data().username;
-
-    return next();
-  } catch (err) {
-    console.error('Error while verifying token', err);
-    return response.status(403).json(err);
-  }
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      request.user = decodedToken;
+      return db
+        .collection('users')
+        .where('userId', '==', request.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      request.user.username = data.docs[0].data().username;
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying token', err);
+      return response.status(403).json(err);
+    });
 };
