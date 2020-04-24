d79667d145b1fb6d5354c59fdd39de992f2e1683
AlexRatmansky <alexratmansky@yandex.ru>
2020-04-24 20:13:20 +0300

    Меняет регион на europe-west3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,21 @@
-import * as functions from 'firebase-functions';
-import { deleteTodo, editTodo, getAllTodos, getOneTodo, postOneTodo } from './api/todos';
-import { getUserDetail, loginUser, signUpUser, updateUserDetails } from './api/users';
-import { auth } from './util/auth';
+import * as functions from 'firebase-functions'
+import { deleteTodo, editTodo, getAllTodos, getOneTodo, postOneTodo } from './api/todos'
+import { getUserDetail, loginUser, signUpUser, updateUserDetails } from './api/users'
+import { auth } from './util/auth'
 
-const app = require('express')();
+const app = require('express')()
 
 // Users
-app.post('/signup', signUpUser);
-app.post('/login', loginUser);
-app.get('/user', auth, getUserDetail);
-app.post('/user', auth, updateUserDetails);
+app.post('/signup', signUpUser)
+app.post('/login', loginUser)
+app.get('/user', auth, getUserDetail)
+app.post('/user', auth, updateUserDetails)
 
 // Todos
-app.get('/todos', auth, getAllTodos);
-app.get('/todo/:todoId', auth, getOneTodo);
-app.post('/todo', auth, postOneTodo);
-app.post('/todo/:todoId', auth, editTodo);
-app.delete('/todo/:todoId', auth, deleteTodo);
+app.get('/todos', auth, getAllTodos)
+app.get('/todo/:todoId', auth, getOneTodo)
+app.post('/todo', auth, postOneTodo)
+app.post('/todo/:todoId', auth, editTodo)
+app.delete('/todo/:todoId', auth, deleteTodo)
 
-export const api = functions.https.onRequest(app);
+export const api = functions.region('europe-west3').https.onRequest(app)
